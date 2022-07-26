120b3fd05567c4cad66a87e681ad180b150bd20c
NikolasPerdikis <nikolas.perdikis@gmail.com>
2022-07-26 15:09:35 +0200

    Create Coursera_Python3_Basics_Week_1.py

---
+++ Coursera_Python3_Basics_Week_1.py
@@ -0,0 +1,15 @@
+import turtle
+import math
+star = turtle.Turtle()
+distance = 100
+
+star.left(75)
+star.forward(distance)
+star.right(150)
+star.forward(distance)
+star.right(145)
+star.forward(distance)
+star.right(140)
+star.forward(distance)
+star.right(140)
+star.forward(distance)
